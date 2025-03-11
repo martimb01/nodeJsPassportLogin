0c5e9fdc31a8ff95199e36c19802d05bed051bef
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-03-11 18:41:59 +0000

    Added register and login post routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const bcrypt = require('bcrypt')
 
 const app = express()
 
+const users =[]
+
 //Setting view engine
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: "Martim"})
@@ -16,9 +20,27 @@ app.get('/register', (req,res) => {
     res.render('register.ejs')
 })
 
+//Post to login users
+app.post('/login', (req,res) => {
+
+})
+
 //Post to register users
-app.post('/register', (req,res) => {
-    
+app.post('/register', async (req,res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString,
+            name: req.body.name,
+            password: hashedPassword,
+            email: req.body.password
+
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('register')
+    }
+    console.log(users)
 })
 
 
